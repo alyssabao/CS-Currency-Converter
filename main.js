e96e1de03360d5afef7badd206aac75f690b1498
alyssabao <64719777+alyssabao@users.noreply.github.com>
2020-05-12 01:12:27 +0700

    added if else statements

--- main.js
+++ main.js
@@ -25,3 +25,52 @@ function usdToVnd(amountUsd) {
 function usdToEur(amountUsd) {
     return (amountUsd / ratioEurToUsd);
 }
+
+function formatUsd(type, value) {
+    const formatter = new Intl.NumberFormat(type, {
+      currency: 'USD',
+      style: "currency"
+    });
+    return formatter.format(value);
+  }
+  
+  function formatVnd(type, value) {
+    const formatter = new Intl.NumberFormat(type, {
+      currency: 'VND',
+      style: "currency"
+    });
+    return formatter.format(value);
+  }
+  
+  function formatEur(type, value) {
+    const formatter = new Intl.NumberFormat(type, {
+      currency: 'EUR',
+      style: "currency"
+    });
+    return formatter.format(value);
+  }
+
+  let fromCurrency = prompt("Which currency do you currently have? VND, USD, or EUR?");
+  let amount = prompt("How much money do you want to convert?")
+  let toCurrency = prompt("Which currency would you like to convert to? VND, USD, or EUR?")
+
+if (fromCurrency === "VND" && toCurrency === "USD") {
+    text =amount + " "+ fromCurrency + " in " + toCurrency + " is  " + formatUsd('en-US',vndToUsd(amount))
+}
+else if (fromCurrency === "VND" && toCurrency === "EUR") {
+    text =amount + " "+ fromCurrency + " in " + toCurrency + " is  " + formatEur('de-DE',vndToEur(amount))
+}
+else if (fromCurrency === "EUR" && toCurrency === "VND") {
+    text =amount + " "+ fromCurrency + " in " + toCurrency + " is  " + formatVnd('vi-VN',eurToVnd(amount))
+}
+else if (fromCurrency === "EUR" && toCurrency === "USD") {
+    text =amount + " "+ fromCurrency + " in " + toCurrency + " is  " + formatUsd('en-US',eurToUsd(amount))
+}
+else if (fromCurrency === "USD" && toCurrency === "VND") {
+    text =amount + " "+ fromCurrency + " in " + toCurrency + " is  " + formatVnd('vi-VN',usdToVnd(amount))
+}
+else if (fromCurrency === "USD" && toCurrency === "EUR") {
+    text =amount + " "+ fromCurrency + " in " + toCurrency + " is  " + formatEur('de-DE',usdToEur(amount))
+}
+
+
