68fe2f48b03211d38297553aadb142b31d8e8676
alyssabao <64719777+alyssabao@users.noreply.github.com>
2020-05-12 13:22:56 +0700

    added NaN alert

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const ratioEurToUsd = 1.08;
 let result = 0;
 let formatAmount = "";
 let fromCurrency = prompt("Which currency do you currently have? VND, USD, or EUR?");
-let amount = prompt("How much money do you want to convert?")
 let toCurrency = prompt("Which currency would you like to convert to? VND, USD, or EUR?")
+let amount = prompt("How much money do you want to convert?")
+let checkNumber = parseInt(amount)
 
 function vndToUsd() {
     return (amount / ratioUsdToVnd);
@@ -33,33 +34,36 @@ function usdToEur() {
 
 function formatCurrency(type, value) {
     const formatter = new Intl.NumberFormat(type, {
-      currency: type,
-      style: "currency"
+        currency: type,
+        style: "currency"
     });
     return formatter.format(value);
-  }  
+}
 
-function converter () {
+function converter() {
     fromCurrency = fromCurrency.toUpperCase();
     toCurrency = toCurrency.toUpperCase();
-    if(fromCurrency === "VND" && toCurrency === "USD") {
-        result = vndToUsd()
-    } else if(fromCurrency === "VND" && toCurrency === "EUR") {
-        result = vndToEur()
-    } else if(fromCurrency === "EUR" && toCurrency === "VND") {
-        result = eurToVnd()
-    } else if (fromCurrency === "EUR" && toCurrency === "USD") {
-        result = eurToUsd()
-    } else if (fromCurrency === "USD" && toCurrency === "VND") {
-        result = usdToVnd()
-    } else if (fromCurrency === "USD" && toCurrency === "EUR") {
-        result = usdToEur()
-    } else {
-        alert("Sorry, your currency is not supported. Please refresh and try again.")
-        return;
+        if (Object.is(checkNumber, NaN)) {
+            alert("Please input a valid number.")
+            return;
+        } else if (fromCurrency === "VND" && toCurrency === "USD") {
+            result = vndToUsd()
+        } else if (fromCurrency === "VND" && toCurrency === "EUR") {
+            result = vndToEur()
+        } else if (fromCurrency === "EUR" && toCurrency === "VND") {
+            result = eurToVnd()
+        } else if (fromCurrency === "EUR" && toCurrency === "USD") {
+            result = eurToUsd()
+        } else if (fromCurrency === "USD" && toCurrency === "VND") {
+            result = usdToVnd()
+        } else if (fromCurrency === "USD" && toCurrency === "EUR") {
+            result = usdToEur()
+        } else {
+            alert("Sorry, your currency is not supported. Please refresh and try again.")
+            return;
+        }
+        formatAmount = formatCurrency(toCurrency, result)
+        alert("The converted amount is: " + formatAmount);
     }
-    formatAmount = formatCurrency(toCurrency,result)
-    alert("The converted amount is: " + formatAmount);
-}
 
 converter()
